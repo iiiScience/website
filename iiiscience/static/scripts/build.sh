13aa3b84358367e8be8241af65e2e701c768f126
William Mayor <mail@williammayor.co.uk>
2013-02-28 21:57:54 +0000

    Minimising for master branch

--- iiiscience/static/scripts/build.sh
+++ iiiscience/static/scripts/build.sh
@@ -2,7 +2,7 @@
 
 java -jar compiler.jar \
 	--js _jquery-1.*.js \
-    --js _jquery.autoSuggest.js \
+    --js _jquery.autocomplete.js \
 	--js _results.js \
 	--js _upload.js \
 	--js _alertify.js \
